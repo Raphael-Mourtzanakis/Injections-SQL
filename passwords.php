7df72cffdb70551ae5484424f4923e1bcde00e27
Raphaël Mourtzanakis <raphael.mourtzanakis@lmdsio.fr>
2025-10-17 09:18:26 +0200

    Version 5.1

--- passwords.php
+++ passwords.php
@@ -3,5 +3,4 @@
 	echo "<b>que20 | monsupermotdepasseintrouvable :</b> ".hash("md2","monsupermotdepasseintrouvable")."<br><br>";
 	echo "<b>clementine | orange :</b> ".hash("md2","orange")."<br><br>";
 	echo "<b>sio | thebestwebsite :</b> ".hash("md2","thebestwebsite")."<br><br>";
-    echo "<b>.admin | cacahouète :</b> ".hash("md2","cacahouète");
 ?>
